b0fa750602823484c9725798ecf41cacea0ef2ce
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2022-03-05 11:37:56 -0300

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,15 @@
+// função
+
+const calc = (e)==> {
+       let a = document.getElementById("primeiro valor").value;
+       let b = document.getElementById("segundo valor").value;
+       let local = document.getElementById("valor somado");
+
+      if (!a){a = 0}
+      if (!b){b = 0}
+
+      result = eval(a+e+b);
+
+      if(isNaN(result)){result = "0"}
+
+      local.innerText = result;
